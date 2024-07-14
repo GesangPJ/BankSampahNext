c18c720156acb674f3356564bd3dd3784f9a5d6c
Gesang Paudra J <g.paudra@gmail.com>
2024-07-14 21:15:26 +0700

    tambah proteksi halaman harga sampah

--- src/app/dashboard/harga-sampah/page.jsx
+++ src/app/dashboard/harga-sampah/page.jsx
@@ -1,11 +1,32 @@
+'use client'
 
+import { useEffect } from "react"
+
+import { useRouter } from "next/navigation"
+
+import { useSession } from "next-auth/react"
 
 import HalamanHargaSampah from "@/views/JenisSampah/hargasampah"
 
 const HargaSampah = () =>{
+  const {data: session, status} = useSession()
+  const router = useRouter()
+
+  useEffect(()=>{
+    if (status === 'loading') return
+
+    if(!session){
+      router.push('/error/401')
+    }
+  }, [session, status, router])
+
+  if(!session){
+    return null
+  }
 
   return(
     <div>
+      <h1>Daftar Harga Sampah Terbaru</h1><br />
       <div>
         <HalamanHargaSampah/>
       </div>
