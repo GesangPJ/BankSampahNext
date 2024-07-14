51d4cf12381d5e511fc623f84296dd76d5117eba
Gesang Paudra J <g.paudra@gmail.com>
2024-07-14 11:27:19 +0700

    update halaman tambah jenis sampah

--- src/app/dashboard/tambah-jenis-sampah/page.jsx
+++ src/app/dashboard/tambah-jenis-sampah/page.jsx
@@ -1,10 +1,33 @@
+"use client"
 
+import { useEffect} from 'react'
+
+import { useRouter } from 'next/navigation'
+
+import { useSession } from 'next-auth/react'
+
+import ViewTambahJenisSampah from "@/views/JenisSampah/tambahjenis"
 
 const HalamanTambahJenisSampah = () =>{
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if(status === 'loading') return
+
+    if(!session){
+      router.push('/error/401')
+    }
+
+  }, [session, status, router])
+
+  if(!session){
+    return null
+  }
 
   return(
     <div>
-
+      <ViewTambahJenisSampah/>
     </div>
   )
 }
