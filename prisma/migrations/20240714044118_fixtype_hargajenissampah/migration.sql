383c6039e0a2ab17b09873181eb5ea8aff03dc4b
Gesang Paudra J <g.paudra@gmail.com>
2024-07-14 13:15:59 +0700

    Delete migration.sql

--- prisma/migrations/20240714044118_fixtype_hargajenissampah/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `hargajenissampah` on the `JenisSampah` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "JenisSampah" DROP COLUMN "hargajenissampah",
-ADD COLUMN     "hargajenissampah" INTEGER NOT NULL;
