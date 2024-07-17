1806e02c23790983828842d1c8c353bef0893d08
Gesang Paudra J <g.paudra@gmail.com>
2024-07-17 12:41:26 +0700

    update halaman tentang

--- src/app/dashboard/tentang/page.jsx
+++ src/app/dashboard/tentang/page.jsx
@@ -11,14 +11,13 @@ const HalamanTentang =() =>{
         </Typography><br />
       </div>
       <div className=" ml-[22px] pl-[22px] text-xl">
-        <p>Nama Website 1 :</p><p className="font-bold"> KASBON MANAGER</p><br />
-        <p>Nama Website 2 :</p><p className="font-bold"> KASBON MANAJER</p><br />
+        <p>Nama Aplikasi :</p><p className="font-bold">BANK SAMPAH NEXT</p><br />
         <p>Pengembang :</p><p className="font-bold">GESANG PAUDRA JAYA, S.Kom</p><hr /><br />
+        <p>Deskripsi : </p><p> Aplikasi pengelola Bank Sampah, berbasis NodeJS dengan framework Next.JS 14 Full Stack.</p>
         <p>Bahasa Pemrograman : JavaScript</p><br />
-        <p>Framework : NextJS v.14.2.3</p><br />
-        <p>Database : PostgreSQL 16 / SQLite 3</p>
-        <p>User Interface : Tailwind & MUI Material UI</p><br />
-
+        <p>Framework : NextJS v.14.2.4</p><br />
+        <p>Database : PostgreSQL 16 / SQLite 3</p><br />
+        <p>Interface Components : Tailwind CSS & MUI Material UI</p><br />
       </div>
 
     </div>
